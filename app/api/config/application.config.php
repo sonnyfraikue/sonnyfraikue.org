2b97c1d3db4abedfb06fb4b394ff230f0de3b585
Sonny Fraikue <sonnyfraikue@gmail.com>
2014-10-15 13:22:46 +0100

    mod

--- app/api/config/application.config.php
+++ app/api/config/application.config.php
@@ -3,45 +3,53 @@ return array(
     'features' => array(
         'txtnation' => array(
             'name' => 'txtNation',
-            'description' => 'HTML5 Boilerplate is a professional front-end template'
-                . ' for building fast, robust, and adaptable web apps or sites.',
+            'description' => 'Maintaining and integrating SMS to http API on client sites like world of Warcraft, Big Brother and Cathy Barry.',
+            'resources'=>'http://www.txtnation.com',
         ),
         'feburman' => array(
             'name' => 'FE Burman Ltd',
-            'description' => 'AngularJS is a toolset for building the framework most'
-                . ' suited to your application development.',
+            'description' => 'Developing web to print systems in mainly php and mysql for Conde Nast and Savills.',
+            'resources'=>'http://www.feburman.co.uk/',
         ),
         'firstclarity' => array(
             'name' => 'First Clarity',
-            'description' => 'Spectacular Test Runner for JavaScript.',
+            'description' => 'Developing pixel perfect CMS driven user interface for Evening Standard Newspaper.',
+            'resources'=>'http://www.firstclarity.com/first-clarity/',
         ),
         'katchup' => array(
             'name' => 'Katchup',
-            'description' => 'Spectacular Test Runner for JavaScript.',
+            'description' => 'Building web applications in JavaScript and jQuery.',
+            'resources'=>'https://www.katchup.com/#/login',
         ),
         'riverisland' => array(
             'name' => 'River Island',
-            'description' => 'Spectacular Test Runner for JavaScript.',
+            'description' => 'Developing responsive, fluid, user-centric multi-device user interface.',
+            'resources'=>'riverisland.co.uk',
         ),
         'prodirect' => array(
             'name' => 'Pro Direct Sport',
-            'description' => 'Spectacular Test Runner for JavaScript.',
+            'description' => 'Implementing front-end MVC and responsive with angular.js for data modelling and handling touch gestures with hammer.js.',
+            'resources'=>'http://www.prodirectselect.com/',
         ),
         'thelearningclinic' => array(
             'name' => 'The Learning Clinic Ltd',
-            'description' => 'Spectacular Test Runner for JavaScript.',
+            'description' => 'Working on the user experience for an NHS "patient monitoring app", I developed angular directives that convert values from percentage to radian in order to be able to represent data as an animated fuel gauge, with the help of some css3 and html5.',
+            'resources'=>'http://www.thelearningclinic.co.uk/',
         ),
         'fcbinferno' => array(
             'name' => 'FCB Inferno',
-            'description' => 'Spectacular Test Runner for JavaScript.',
+            'description' => 'Pending.',
+            'resources'=>'http://www.fcbinferno.com/',
         ),
         'tmw' => array(
             'name' => 'TMW Agency',
-            'description' => 'Spectacular Test Runner for JavaScript.',
+            'description' => 'Built a responsive mini site for the toyota infiniti brand, developed using grunt, yeoman and bower.',
+            'resources'=>'http://www.tmw.co.uk/',
         ),
         'movingbrands' => array(
             'name' => 'Moving Brands',
-            'description' => 'Spectacular Test Runner for JavaScript.',
+            'description' => 'Wrote some slim markup and css for the Omega brand',
+            'resources'=>'http://www.movingbrands.com/',
         ),
     ),
 );
